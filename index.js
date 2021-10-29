11c11b857f5407a48bef0b2e8c1fd1ba5fcb18ff
Goldstar32 <josua.karnsund@gmail.com>
2021-10-29 11:45:56 +0200

    Relativ storlek

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var app = new Vue({
     el: '#app',
     data: {
-        duckX: 300,
-        duckY: 300,
+        duckX: 0,
+        duckY: 0,
         ducksShot: 0,
     },
 
@@ -19,8 +19,15 @@ var app = new Vue({
     methods: {
 
         rndDuckPos() {
-            var x = this.getRndInteger(300, 500)
-            var y = this.getRndInteger(300, 500)
+            var screenWitdth = this.$refs.duckContainer.clientWidth;
+            var screenHeight = this.$refs.duckContainer.clientHeight;
+            var duckSize = 140;
+            var minX = 0 + duckSize / 2
+            var minY = 0 + duckSize / 2
+            var maxX = screenWitdth - duckSize / 2
+            var maxY = screenHeight - duckSize / 2
+            var x = this.getRndInteger(minX, maxX)
+            var y = this.getRndInteger(minY, maxY)
             this.duckX = x
             this.duckY = y
             this.ducksShot += 1
